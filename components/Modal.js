bf00217ba88300e981435de108a8f13314201cb7
NaokiYazawa <yazawa.naoki.01@gmail.com>
2021-12-19 00:02:21 +0900

    set up comment modal

--- components/Modal.js
+++ components/Modal.js
@@ -29,13 +29,13 @@ function Modal() {
   const [comment, setComment] = useState("");
   const router = useRouter();
 
-  // useEffect(
-  //   () =>
-  //     onSnapshot(doc(db, "posts", postId), (snapshot) => {
-  //       setPost(snapshot.data());
-  //     }),
-  //   [db]
-  // );
+  useEffect(
+    () =>
+      onSnapshot(doc(db, "posts", postId), (snapshot) => {
+        setPost(snapshot.data());
+      }),
+    [db]
+  );
 
   const sendComment = async (e) => {
     e.preventDefault();
@@ -87,6 +87,81 @@ function Modal() {
                   onClick={() => setIsOpen(false)}
                 >
                   <XIcon className="h-[22px] text-white" />
+                </div>
+              </div>
+              <div className="flex px-4 pt-5 pb-2.5 sm:px-6">
+                <div className="w-full">
+                  <div className="text-[#6e767d] flex gap-x-3 relative">
+                    <span className="w-0.5 h-full z-[-1] absolute left-5 top-11 bg-gray-600" />
+                    <img
+                      src={post?.userImg}
+                      alt=""
+                      className="h-11 w-11 rounded-full"
+                    />
+                    <div>
+                      <div className="inline-block group">
+                        <h4 className="font-bold text-[#d9d9d9] inline-block text-[15px] sm:text-base">
+                          {post?.username}
+                        </h4>
+                        <span className="ml-1.5 text-sm sm:text-[15px]">
+                          @{post?.tag}{" "}
+                        </span>
+                      </div>{" "}
+                      ·{" "}
+                      <span className="hover:underline text-sm sm:text-[15px]">
+                        <Moment fromNow>{post?.timestamp?.toDate()}</Moment>
+                      </span>
+                      <p className="text-[#d9d9d9] text-[15px] sm:text-base">
+                        {post?.text}
+                      </p>
+                    </div>
+                  </div>
+
+                  <div className="mt-7 flex space-x-3 w-full">
+                    <img
+                      src={session.user.image}
+                      alt=""
+                      className="h-11 w-11 rounded-full"
+                    />
+                    <div className="flex-grow mt-2">
+                      <textarea
+                        value={comment}
+                        onChange={(e) => setComment(e.target.value)}
+                        placeholder="Tweet your reply"
+                        rows="2"
+                        className="bg-transparent outline-none text-[#d9d9d9] text-lg placeholder-gray-500 tracking-wide w-full min-h-[80px]"
+                      />
+
+                      <div className="flex items-center justify-between pt-2.5">
+                        <div className="flex items-center">
+                          <div className="icon">
+                            <PhotographIcon className="text-[#1d9bf0] h-[22px]" />
+                          </div>
+
+                          <div className="icon rotate-90">
+                            <ChartBarIcon className="text-[#1d9bf0] h-[22px]" />
+                          </div>
+
+                          <div className="icon">
+                            <EmojiHappyIcon className="text-[#1d9bf0] h-[22px]" />
+                          </div>
+
+                          <div className="icon">
+                            <CalendarIcon className="text-[#1d9bf0] h-[22px]" />
+                          </div>
+                        </div>
+
+                        <button
+                          className="bg-[#1d9bf0] text-white rounded-full px-4 py-1.5 font-bold shadow-md hover:bg-[#1a8cd8] disabled:hover:bg-[#1d9bf0] disabled:opacity-50 disabled:cursor-default"
+                          type="submit"
+                          onClick={sendComment}
+                          disabled={!comment.trim()}
+                        >
+                          Reply
+                        </button>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
